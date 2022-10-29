eb2983c15e9312470c5813cb490826a1365f9cbf
Khizi14 <khizi72@gmail.com>
2022-10-29 23:33:25 +0500

    Project Update

---
+++ Assignment1/Assg1/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'goal',
+    loadChildren: () => import('./pages/goal/goal.module').then( m => m.GoalPageModule)
+  },
+  {
+    path: 'critique',
+    loadChildren: () => import('./pages/critique/critique.module').then( m => m.CritiquePageModule)
+  },
+  {
+    path: 'personas',
+    loadChildren: () => import('./pages/personas/personas.module').then( m => m.PersonasPageModule)
+  },
+  {
+    path: 'sketches',
+    loadChildren: () => import('./pages/sketches/sketches.module').then( m => m.SketchesPageModule)
+  },
+  {
+    path: 'tecnologies',
+    loadChildren: () => import('./pages/tecnologies/tecnologies.module').then( m => m.TecnologiesPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
